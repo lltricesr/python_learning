24a5c720d933bd03dde7b7d3824b10682f2ddba2
Leonar Trice Sr <lltricesr@gmail.com>
2024-02-18 21:34:48 -0500

    [wip]starter code review.

--- WorkingWithDatabases/main.py
+++ WorkingWithDatabases/main.py
@@ -15,9 +15,25 @@ import click
     get(url) -> get request to the url
     .json() -> because the content-type in the response is application/json; used to parse response into a json dict
     data -> parsed dictionary from the json
+
+    decorators
+        @click.command() -> to mark for CLI enabled
+        @click.option("opt", default="val") -> parse the options from the CLI; adding the default makes the option optional
 """
+@click.command()
+@click.option("--coin_id", default="bitcoin")
+@click.option("--currency", default="usd")
 def get_coin_price(coin_id,currency):
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={currency}"
     data = requests.get(url).json()
     coin_price = data[coin_id][currency]
     print(f"The price of {coin_id} is {coin_price:.2f} {currency.upper()}")
+
+"""
+    special dunder method: __main__ called at the execution/initialization of the module
+        this special function is executed behind the scene in all modules
+    special dunder method: __name__ can be used to "intercept" the call to __main__ to provide additional checking
+        good to use for stoping the auto execution of __main__ when imported into other modules
+"""
+if __name__ == "__main__":
+    get_coin_price()
